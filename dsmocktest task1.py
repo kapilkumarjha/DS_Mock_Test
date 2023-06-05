16d77af72ad163ff2ab98b7c29b11e824ac1a4c6
Kapil Kumar Jha <94006670+kapilkumarjha@users.noreply.github.com>
2023-06-05 10:17:55 +0530

    Update dsmocktest task1.py

--- dsmocktest task1.py
+++ dsmocktest task1.py
@@ -1,14 +1,14 @@
-# def move_zero(nums):
-#  n = len(nums)
-#  j = 0
-#  for i in range(n):
-#   if nums[i] != 0:
-#    nums[i], nums[j] = nums[j], nums[i]
-#    j += 1
-#
-# nums = [0,1,0,3,12]
-# move_zero(nums)
-# print(nums)
+def move_zero(nums):
+ n = len(nums)
+ j = 0
+ for i in range(n):
+  if nums[i] != 0:
+   nums[i], nums[j] = nums[j], nums[i]
+   j += 1
+
+nums = [0,1,0,3,12]
+move_zero(nums)
+print(nums)
 ############################################################################################################################
 def firstUniqChar(s):
  char_freq = {}
